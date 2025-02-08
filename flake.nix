168d611ccb29f64aec57410b1c399c74cee8cddf
arsmoriendy <nugrahasteven@gmail.com>
2025-02-08 17:06:46 +0700

    feat: setup flake dev environment

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Flake for developing TagFS";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+    {
+      devShells."x86_64-linux".default = pkgs.mkShell {
+        packages = [
+          pkgs.rustup
+          pkgs.pkg-config
+          pkgs.fuse3
+        ];
+        # LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [ pkgs.fuse3 ]}";
+        # C_INCLUDE_PATH = "${pkgs.lib.makeIncludePath [
+        #   pkgs.fuse3
+        #   pkgs.glibc
+        # ]}";
+      };
+    };
+}

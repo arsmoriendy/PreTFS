f51fc7b4f53a58f9807f94218b55bb9912a1c766
arsmoriendy <nugrahasteven@gmail.com>
2025-02-10 17:53:09 +0700

    feat: test setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+#[cfg(test)]
+mod test {
+    struct Setup<'a> {
+        monut_path: &'a str,
+    }
+
+    impl Default for Setup<'_> {
+        fn default() -> Self {
+            let ret = Setup {
+                monut_path: "mountpoint",
+            };
+            if let Err(e) = std::fs::create_dir(ret.monut_path) {
+                panic!("{e}");
+            }
+            return ret;
+        }
+    }
+
+    impl Drop for Setup<'_> {
+        fn drop(&mut self) {
+            if let Err(e) = std::fs::remove_dir_all(self.monut_path) {
+                panic!("{e}");
+            }
+        }
+    }
+
+    #[test]
+    fn mount() {
+        Setup::default();
+    }
 }

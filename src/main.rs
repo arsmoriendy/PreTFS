d287a00a1a8cf4e9ca81446fa8baab330c2be489
arsmoriendy <nugrahasteven@gmail.com>
2025-02-15 10:38:14 +0700

    feat: wip mountpath attrs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::{Duration, SystemTime};
+
 use async_std::task;
 use libc::c_int;
 
@@ -15,6 +17,43 @@ impl Filesystem for TagFileSystem {
 
     fn destroy(&mut self) {
         task::block_on(self.pool.close());
+    }
+
+    fn getattr(&mut self, _req: &Request<'_>, ino: u64, _fh: Option<u64>, reply: ReplyAttr) {
+        // inode 1 is the mountpoint directory itself
+        if ino == 1 {
+            return reply.attr(
+                &Duration::from_secs(1),
+                &FileAttr {
+                    ino: 1,
+                    nlink: 1,
+                    rdev: 0,
+
+                    // TODO: size related
+                    size: 0,
+                    blocks: 0,
+
+                    // TODO: time related
+                    atime: SystemTime::UNIX_EPOCH,
+                    mtime: SystemTime::UNIX_EPOCH,
+                    ctime: SystemTime::UNIX_EPOCH,
+                    crtime: SystemTime::UNIX_EPOCH,
+
+                    kind: FileType::Directory,
+
+                    // TODO: permission related, sync with original dir mayhaps?
+                    perm: 0b_111_101_101, // rwx r-x r-x
+
+                    // TODO: user/group related
+                    uid: _req.uid(),
+                    gid: _req.gid(),
+
+                    // TODO: misc
+                    blksize: 0,
+                    flags: 0,
+                },
+            );
+        }
     }
 }
 

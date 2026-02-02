ddec02feba4de8f16059a3512b55053b7907a490
arsmoriendy <nugrahasteven@gmail.com>
2026-02-02 09:41:25 +0700

    chore: format migrations

--- migrations/3_readdir-rows.sql
+++ migrations/3_readdir-rows.sql
@@ -1,3 +1,20 @@
 CREATE VIEW IF NOT EXISTS readdir_rows AS
-SELECT * FROM file_attrs
+SELECT
+  file_attrs.ino,
+  file_attrs.size,
+  file_attrs.blocks,
+  file_attrs.atime,
+  file_attrs.mtime,
+  file_attrs.ctime,
+  file_attrs.crtime,
+  file_attrs.kind,
+  file_attrs.perm,
+  file_attrs.nlink,
+  file_attrs.uid,
+  file_attrs.gid,
+  file_attrs.rdev,
+  file_attrs.blksize,
+  file_attrs.flags,
+  file_names.name
+FROM file_attrs
 INNER JOIN file_names ON file_attrs.ino = file_names.ino;
--- migrations/7_file-contents.sql
+++ migrations/7_file-contents.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS file_contents (
   page INTEGER,
   bytes BLOB,
   PRIMARY KEY (ino, page),
-  FOREIGN KEY (ino) REFERENCES file_attrs(ino)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE
+  FOREIGN KEY (ino) REFERENCES file_attrs (ino)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE
 );
